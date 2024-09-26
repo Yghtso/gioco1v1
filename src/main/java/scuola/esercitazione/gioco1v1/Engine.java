a755be44a6c37cabc5b37309f0b4c7792bf2a533
Cristian Soare <soare.cristian@outlook.com>
2024-09-26 14:55:54 +0000

    Aggiunti i primi messaggi di log della connessione tra i giocatori

--- src/main/java/scuola/esercitazione/gioco1v1/Engine.java
+++ src/main/java/scuola/esercitazione/gioco1v1/Engine.java
@@ -1,5 +1,35 @@
 package scuola.esercitazione.gioco1v1;
 
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Engine {
-    
+
+    private Socket sock;
+    private Player player;
+
+    public Engine(Socket sock, Player player) {
+        this.sock = sock;
+        this.player = player;
+    };
+
+    void start() {
+        Scanner s = new Scanner(System.in);
+        if (this.player == Player.white) {
+            System.out.println("Inserisci un una mossa : ");
+            s.nextLine();
+        }
+        s.close();
+    }
+
+    // FUNZIONE CHE SERVE A MAPPARE LA MOSSA(STRINGA) ALLA MOSSA EFFETTIVA
+    // SOLO TEMPORANEA FINO A QUANDO NON SARA' GRAFICO, NON A LINEA DI COMANDO
+    void mapToMove(String move) {
+
+    }
+}
+
+enum Player {
+    white,
+    black,
 }
--- src/main/java/scuola/esercitazione/gioco1v1/Gioco1v1.java
+++ src/main/java/scuola/esercitazione/gioco1v1/Gioco1v1.java
@@ -3,8 +3,7 @@ package scuola.esercitazione.gioco1v1;
 public class Gioco1v1 {
 
     public static void main(String[] args) {
-        UI ui = new UI();
-
-        ui.start();
+        Game gioco = new Game();
+        gioco.start();
     }
 }
