4fd4b9ec6d7bb8a07b1009b86aa5196be123b90b
Cristian Soare <soare.cristian@outlook.com>
2024-10-17 18:50:20 +0200

    Aggiunto il plugin per le applicazioni javafx

--- src/main/java/scuola/esercitazione/gioco1v1/Gioco1v1.java
+++ src/main/java/scuola/esercitazione/gioco1v1/Gioco1v1.java
@@ -1,8 +1,32 @@
 package scuola.esercitazione.gioco1v1;
 
-public class Gioco1v1 {
-    public static void main(String args[]) {
-        Game gioco = new Game();
-        gioco.start();
-    }    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Gioco1v1 extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Schermata Scacchiera.fxml"));
+            Parent root = loader.load();
+
+            // Create the scene
+            Scene scene = new Scene(root);
+
+            // Set the stage
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("JavaFX FXML Example");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
