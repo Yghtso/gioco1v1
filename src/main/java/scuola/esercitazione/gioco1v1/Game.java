fd65298f0738728a83d8eb64fafa708b4b28ef76
Cristian Soare <soare.cristian@outlook.com>
2024-10-23 22:52:59 +0200

    Finire la migrazione verso la classe UIManager

--- src/main/java/scuola/esercitazione/gioco1v1/Game.java
+++ src/main/java/scuola/esercitazione/gioco1v1/Game.java
@@ -20,122 +20,39 @@ public class Game {
     private Player player = Player.WHITE;
     private MoveChecker checker = new MoveChecker(board);
 
-     @FXML
-    private AnchorPane AnchorPaneIniziale;
     @FXML
-    private Button PlayButton;
-    @FXML
-    private Button QuitButton;
-    @FXML
-    private Button ZanoButton;
-    @FXML
-    private ImageView Logo;
-
-    @FXML
-    public void selectPiece(MouseEvent event) {
-        Pane clickedBox = (Pane) event.getSource();
-
-        Integer row = GridPane.getRowIndex(clickedBox) == null ? 8 : 8 - GridPane.getRowIndex(clickedBox);
-        Integer column = GridPane.getColumnIndex(clickedBox) == null ? 1 : GridPane.getColumnIndex(clickedBox) + 1;
-        Position pos = new Position(row.intValue(), column.intValue());
-
-        Piece piece = board.getPiece(pos);
-        Player pieceOwner = piece == null ? null : piece.getOwner();
-        boolean clickedOwnedPiece = pieceOwner == this.player;
-
-        // STO SELEZIONANDO UNA PEDINA PER LA PRIMA VOLTA E SELEZIONO UNA PEDINA NON MIA
-        if (!isPieceSelected && clickedOwnedPiece) {
-
-            this.isPieceSelected = true;
-            this.selectedPiece = piece;
-            // TODO: far vedere sulla scacchiera le mosse possibili
-            return;
-        }
-
-        if (isPieceSelected && clickedOwnedPiece) {
-
-            // TODO: CHECK DELL ARROCCO DA FARE
-            this.isPieceSelected = true;
-            this.selectedPiece = piece;
-            // TODO: far vedere sulla scacchiera le mosse possibili
-            return;
-        }
-
-        if (isPieceSelected && !clickedOwnedPiece) {
-
-            Move move = new Move(new Position(row, column), selectedPiece, false);
-            selectedPiece.calculateMoves();
-
-            for (Move singlMove : selectedPiece.getValidMoves()) {
-                System.out.println("Mossa possibile : " + singlMove.getPosition().getRow() + ", " + singlMove.getPosition().getColumn());
-            }
-            System.out.println("Mosse valide :");
-            checker.checkMoves(selectedPiece.getValidMoves());
-            for (Move singlMove : selectedPiece.getValidMoves()) {
-                System.out.println("Mossa possibile : " + singlMove.getPosition().getRow() + ", " + singlMove.getPosition().getColumn());
-            }
-
-            for (Move singleMove : selectedPiece.getValidMoves()) {
-                if (singleMove.equals(move)) {
-                    System.out.println("Mossa valida");
-                }
-            }
-            this.isPieceSelected = false;
-            this.selectedPiece = null;
-            return;
-        }
+    public void selectPiece(MouseEvent event) {        
     }
 
-    @FXML
-    public void arrenditi(ActionEvent event) {
-        System.out.println("Ti sei arreso");
-    }
-    @FXML
-    public void PlayButton(ActionEvent event) {
-
-        //PlayButton.setVisible(false);
-        //QuitButton.setVisible(false);
-        //ZanoButton.setVisible(true);
-
-        Animazione();
-
+    public void setIsPieceSelected (boolean isPieceSelected) {
+        this.isPieceSelected = isPieceSelected;
     }
 
-    public void Animazione(){
-
-        TranslateTransition AnimazioneLogo= new TranslateTransition(Duration.seconds(1), Logo);
-
-        double centerXLogo = (Logo.getScene().getWidth() - Logo.getFitWidth()) / 2;
-        double centerXButton = (Logo.getScene().getWidth() -PlayButton.getWidth()) / 2;
-        double centerYButton = (Logo.getScene().getHeight()-PlayButton.getWidth()) / 2;
-
-        AnimazioneLogo.setToX(centerXLogo - Logo.getLayoutX());
-        AnimazioneLogo.setToY(-(Logo.getLayoutY() - 50)); 
-
-        TranslateTransition AnimazioneBottone1= new TranslateTransition(Duration.seconds(1), PlayButton);
-
-        AnimazioneBottone1.setToX(centerXButton - PlayButton.getLayoutX()- centerXButton/2);
-        AnimazioneBottone1.setToY(-(centerYButton + PlayButton.getLayoutY())+100);
-
-        TranslateTransition AnimazioneBottone2= new TranslateTransition(Duration.seconds(1),QuitButton);
-
-        AnimazioneBottone2.setToX(centerXButton - PlayButton.getLayoutX()+ centerXButton/2);
-        AnimazioneBottone2.setToY(-(centerYButton + PlayButton.getLayoutY()));
-
-        ParallelTransition parallelTransition = new ParallelTransition(AnimazioneLogo, AnimazioneBottone1, AnimazioneBottone2);
-
-        parallelTransition.play();
-
+    public void setSelectedPiece (Piece selectedPiece) {
+        this.selectedPiece = selectedPiece;
     }
 
-    @FXML
-    void QuitButton(ActionEvent event) {
-
-
+    public ChessBoard getBoard() {
+        return this.board;
     }
 
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public boolean getIsPieceSelected() {
+        return this.isPieceSelected;
+    }
+
+    public Piece getSelectedPiece() {
+        return this.selectedPiece;
+    }
+
+    public MoveChecker getChecker() {
+        return this.checker;
+    }
 }
-
+    
 enum Player {
     WHITE, BLACK,
 }
---
+++ src/main/java/scuola/esercitazione/gioco1v1/UIManager.java
@@ -0,0 +1,124 @@
+package scuola.esercitazione.gioco1v1;
+
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+public class UIManager {
+    
+    Game game = new Game();
+
+    @FXML
+    private AnchorPane AnchorPaneIniziale;
+    @FXML
+    private Button PlayButton;
+    @FXML
+    private Button QuitButton;
+    @FXML
+    private ImageView Logo;
+    
+    @FXML
+    public void clickedSquare(MouseEvent event) {
+        Pane clickedSquare = (Pane) event.getSource();
+
+        Integer row = GridPane.getRowIndex(clickedSquare) == null ? 8 : 8 - GridPane.getRowIndex(clickedSquare);
+        Integer column = GridPane.getColumnIndex(clickedSquare) == null ? 1 : GridPane.getColumnIndex(clickedSquare) + 1;
+        Position pos = new Position(row.intValue(), column.intValue());
+
+        Piece piece = game.getBoard().getPiece(pos);
+        Player pieceOwner = piece == null ? null : piece.getOwner();
+        boolean clickedOwnedPiece = pieceOwner == game.getPlayer();
+
+        if (!game.getIsPieceSelected() && clickedOwnedPiece) {
+
+            game.setIsPieceSelected(true);
+            game.setSelectedPiece(piece);
+            // TODO: far vedere sulla scacchiera le mosse possibili
+            return;
+        }
+
+        
+        if (game.getIsPieceSelected() && clickedOwnedPiece) {
+
+            // TODO: CHECK DELL ARROCCO DA FARE
+            game.setIsPieceSelected(true);
+            game.setSelectedPiece(piece);
+            // TODO: far vedere sulla scacchiera le mosse possibili
+            return;
+        }
+
+        if (game.getIsPieceSelected() && !clickedOwnedPiece) {
+
+            Move move = new Move(new Position(row, column), game.getSelectedPiece(), false);
+            
+            game.getSelectedPiece().calculateMoves();
+            game.getChecker().checkMoves(game.getSelectedPiece().getValidMoves());
+
+            for (Move singleMove : game.getSelectedPiece().getValidMoves()) {
+                if (singleMove.equals(move)) {
+                    System.out.println("Mossa valida");
+                }
+            }
+            game.setIsPieceSelected(false);
+            game.setSelectedPiece(null);
+            return;
+        }
+    }
+
+    @FXML
+    public void PlayButton(ActionEvent event) {
+
+        //PlayButton.setVisible(false);
+        //QuitButton.setVisible(false);
+        //ZanoButton.setVisible(true);
+
+        Animazione();
+
+    }
+
+    public void Animazione(){
+
+        TranslateTransition AnimazioneLogo= new TranslateTransition(Duration.seconds(1), Logo);
+
+        double centerXLogo = (Logo.getScene().getWidth() - Logo.getFitWidth()) / 2;
+        double centerXButton = (Logo.getScene().getWidth() -PlayButton.getWidth()) / 2;
+        double centerYButton = (Logo.getScene().getHeight()-PlayButton.getWidth()) / 2;
+
+        AnimazioneLogo.setToX(centerXLogo - Logo.getLayoutX());
+        AnimazioneLogo.setToY(-(Logo.getLayoutY() - 50)); 
+
+        TranslateTransition AnimazioneBottone1= new TranslateTransition(Duration.seconds(1), PlayButton);
+
+        AnimazioneBottone1.setToX(centerXButton - PlayButton.getLayoutX()- centerXButton/2);
+        AnimazioneBottone1.setToY(-(centerYButton + PlayButton.getLayoutY())+100);
+
+        TranslateTransition AnimazioneBottone2= new TranslateTransition(Duration.seconds(1),QuitButton);
+
+        AnimazioneBottone2.setToX(centerXButton - PlayButton.getLayoutX()+ centerXButton/2);
+        AnimazioneBottone2.setToY(-(centerYButton + PlayButton.getLayoutY()));
+
+        ParallelTransition parallelTransition = new ParallelTransition(AnimazioneLogo, AnimazioneBottone1, AnimazioneBottone2);
+
+        parallelTransition.play();
+
+    }
+
+    @FXML
+    void QuitButton(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    public void arrenditi(ActionEvent event) {
+        System.out.println("Ti sei arreso");
+    }
+}
